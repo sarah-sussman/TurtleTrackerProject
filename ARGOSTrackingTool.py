7690597c7cdde2edb71413cf4feffa9ac05ae03e
sarah-sussman <sarah.sussman@duke.edu>
2024-10-07 12:54:21 -0400

    Building Python dictionaries of ARGOS observations

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,13 +16,19 @@ file_name = './data/raw/sara.txt'
 file_object = open(file_name,'r')
 
 #Read contents of file into a list
-lineString = file_object.readline()
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Initialize dictioinaries
+date_dict= {}
+location_dict = {}
 
 #Pretend we read one line of data from the file
-while lineString:
+for lineString in line_list:
     # Check if line is a data line
     if lineString[0] in ("#","u"):
-        lineString = file_object.readline()
         continue
 
     #Split the string into a list of data items
@@ -35,8 +41,9 @@ while lineString:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
+    #Add items to dictionaries 
+    date_dict[record_id] = obs_date
+    location_dict[record_id] = (obs_lat, obs_lon)
+    
     #Print the location of sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-
-    #Read next line
-    lineString = file_object.readline()
+    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
